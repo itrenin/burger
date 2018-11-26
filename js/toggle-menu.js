4c24927eb5e9f3321f1febae58d5d01f1a166e5e
Ilya Trenin <itrenin@gmail.com>
2018-11-26 04:40:44 +0300

    добавлена блокировка враппера при активации полноэкранного меню

--- js/toggle-menu.js
+++ js/toggle-menu.js
@@ -1,6 +1,7 @@
 
 const hamburger = document.querySelector("#hamburger-menu-button");
 const burgerFullScreenMenu = document.querySelector(".burger-menu-content");
+const wrapperBlock = document.querySelector(".wrapper");
 
 const styleHamburger = getComputedStyle(hamburger);
 
@@ -10,10 +11,12 @@ hamburger.addEventListener("click", function(e){
 
     if (burgerFullScreenMenu.classList.contains("burger-menu-content--active")){
         burgerFullScreenMenu.classList.remove("burger-menu-content--active");
+        wrapperBlock.classList.remove("wrapper--blocked");
         hamburger.classList.remove("hamburger-menu__icon-pic-close");
     }
     else {
         burgerFullScreenMenu.classList.add("burger-menu-content--active");
+        wrapperBlock.classList.add("wrapper--blocked");
         hamburger.classList.add("hamburger-menu__icon-pic-close");
     }
 
